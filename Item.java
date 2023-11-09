aefd0dc90742eafe5f1fad753277dea97587fda3
DeviPavan <devipavan971@gmail.com>
2023-11-09 15:42:25 +0530

    Changes done to the class file.

--- Item.java
+++ Item.java
@@ -1,17 +1,41 @@
-public class Item {
+abstract class Item {
+    void checkout(){
+        System.out.println("Checking out....");
+    }
+    abstract String returnItem();
+   // void returnItem()
+   static {
+        System.out.println("Return Item Details....");
+    }
+    public void displayDetails(){
+        System.out.println("Details....");
+    }
     String Title;
     String Author;
     int year;
     boolean available;
+
     // void checkOut()
     //void returnItem()
-    // void displayDetails();
+    void display() {
+
+    }
 }
 class Book extends Item{
     String NameofBook;
     int noofBooks;
+
+    @Override
+    String returnItem() {
+        return null;
+    }
 }
 class DVD extends Item{
     String NameofDVD;
     int noofDVDs;
+
+    @Override
+    String returnItem() {
+        return null;
+    }
 }
