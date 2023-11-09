42210f21bb481b9335e8dc6d22982941d5f6b074
DeviPavan <devipavan971@gmail.com>
2023-11-09 13:43:07 +0530

    Created base class, child class & inherited.

---
+++ Item.java
@@ -0,0 +1,17 @@
+public class Item {
+    String Title;
+    String Author;
+    int year;
+    boolean available;
+    // void checkOut()
+    //void returnItem()
+    // void displayDetails();
+}
+class Book extends Item{
+    String NameofBook;
+    int noofBooks;
+}
+class DVD extends Item{
+    String NameofDVD;
+    int noofDVDs;
+}
